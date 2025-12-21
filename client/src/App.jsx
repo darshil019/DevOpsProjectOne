24da6e9b88b05ad8a0b743c13ae04f7a7090328b
darshil019 <panditdarshil5454@gmail.com>
2025-12-21 19:14:35 +0530

    checking ci 5 fixing lint issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,9 +12,6 @@ function App() {
   const [error, setError] = useState(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  let darshil = "hello";
-  let h = "hey";
-
   // Fetch tasks on component mount
   useEffect(() => {
     fetchTasks();
